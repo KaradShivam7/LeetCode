8068dc813c35b66454e09443f81c1f1fee3e715a
KaradShivam7 <shivamkarad20@gmail.com>
2026-03-14 22:25:38 +0530

    java

--- problem35.java
+++ problem35.java
@@ -1,9 +1,9 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
     
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]>=target){
-               return j;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+               return i;
             }
         }
         return nums.length;
